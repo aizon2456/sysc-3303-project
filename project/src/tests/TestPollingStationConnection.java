28cc4cb7be8fc8ed9392e0491a55775bd6ff277a
Rosengren <kev.rosengren@gmail.com>
2015-03-17 19:06:51 -0400

    added TestSuite

---
+++ project/src/tests/TestPollingStationConnection.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import pollingStation.PollingStationConnection;
+
+public class TestPollingStationConnection {
+
+    private PollingStationConnection connection;
+
+    @Before
+    public void setUp() {
+        connection = new PollingStationConnection();
+    }
+
+    @Test
+    public void testMethod() {
+
+    }
+
+}
