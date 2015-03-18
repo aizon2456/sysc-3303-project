a4122e5487068fb540752b4435f1fc2b1cb54e7d
Nikola Neskovic <nick_neskov@Hotmail.com>
2015-03-18 00:47:03 -0400

    Biggest change so far

--- project/src/districtServer/View.java
+++
@@ -1,5 +0,0 @@
-package districtServer;
-
-public class View {
-
-}
--- project/src/districtServer/Voter.java
+++ project/src/districtServer/Voter.java
@@ -1,5 +1,80 @@
 package districtServer;
 
-public class Voter {
+public class Voter extends Person {
 	
+	private String district, loginName, password;
+	private boolean registered, voted;
+
+	public Voter(String firstName, String lastName, String socialInsuranceNumber, String district){
+		super(firstName, lastName, socialInsuranceNumber);
+		this.district = district;
+	}
+	
+	public boolean vote(){
+		return false;
+	}
+	
+	public boolean isRegistered() {
+		return registered;
+	}
+
+	public void setRegistered(boolean registered) {
+		this.registered = registered;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public boolean hasVoted() {
+		return voted;
+	}
+
+	public void setVoted(boolean voted) {
+		this.voted = voted;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		Voter voter;
+		if(obj instanceof Voter)
+			voter = (Voter)obj;
+		else
+			return false;
+		
+		//Compares ALL of the attributes of Voter to ensure the two are entirely equal.
+		if(!(this.socialInsuranceNumber.equals(voter.getSocialInsuranceNumber())))
+			return false;
+		if(!(this.loginName.equals(voter.getLoginName())))
+			return false;
+		if(!(this.password.equals(voter.getPassword())))
+			return false;
+		if(!(this.registered == voter.isRegistered()))
+			return false;
+		if(!(this.voted == voter.hasVoted()))
+			return false;
+		
+		return true;
+	}
 }
