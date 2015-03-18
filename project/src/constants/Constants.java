249c561a9f5cc3a2305c55e0e8e57531ce9030b9
ianwong <ian.m.y.wong@gmail.com>
2015-03-18 17:12:41 -0400

    Constants fixes

--- project/src/constants/Constants.java
+++ project/src/constants/Constants.java
@@ -1,7 +1,5 @@
 package constants;
 
-import java.util.ArrayList;
-
 /**
  * Constants class.
  */
@@ -16,7 +14,6 @@ public final class Constants {
 	 * input before assuming that the election is over. 
 	 */
 	public static final int ELECTION_OVER 		= 30000; // 30 seconds
-	public static final int TEST_ELECTION_OVER	= 5000; // 5 seconds
         
     public static enum packetType {
         REGISTER, LOGIN, VOTE, RESULT, UPDATE, NO_RESPONSE
@@ -25,31 +22,5 @@ public final class Constants {
     public static enum returnCodes {
         SUCCESS, NON_EXISTENT, ALREADY_REGISTERED,
         WRONG_CREDENTIALS, ALREADY_VOTED, VOTING_DONE
-    }
-    
-    /**
-     * Parses through a pipe-delimited string and returns an array of Strings
-     * @param piped The input string
-     * @return The non-piped parsed string array
-     */
-    public static String[] parsePipedString(String piped) {
-    	ArrayList<String> parse = new ArrayList<String>();
-    	
-    	String words = "";
-    	for (int i = 0; i < piped.length(); i++) {
-    		char curr = piped.charAt(i);
-    		if (curr == '\0') {
-    			break;
-    		}
-    		else if (curr == '|') {
-    			parse.add(words);
-    			words = "";
-    		}
-    		else {
-    			words += curr;
-    		}
-    	}
-    	
-    	return (String[]) parse.toArray();
     }
 }
