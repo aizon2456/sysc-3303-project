1308616651ddc01b2f701ae45f35f2222fa73c18
Nikola Neskovic <nick_neskov@Hotmail.com>
2015-03-18 17:07:55 -0400

    Minor changes

--- project/src/constants/Constants.java
+++ project/src/constants/Constants.java
@@ -9,15 +9,15 @@ public final class Constants {
     public static final int PORT_SIZE = 4;
     public static final int DATA_SIZE = 256;
     public static final int PACKET_SIZE = SEND_SIZE + DATA_SIZE;
-    public static final char PACKET_DELIMITER = '|';
     public static final char PACKET_END = '\0';
+    public static final String PACKET_DELIMITER = "|";
+    public static final String IPV4_REGEX = "\\A(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}\\z";
 
     public static enum packetType {
         REGISTER, LOGIN, VOTE, RESULT, UPDATE, NO_RESPONSE
     }
 
     public static enum returnCodes {
-        SUCCESS, NON_EXISTENT, ALREADY_REGISTERED,
-        WRONG_CREDENTIALS, ALREADY_VOTED
+        SUCCESS, NON_EXISTENT, ALREADY_REGISTERED, WRONG_CREDENTIALS, ALREADY_VOTED, LOGIN_EXISTS
     }
 }
