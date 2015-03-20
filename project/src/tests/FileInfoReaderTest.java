e6eedfa479c9354efd83ee2bd6d9a043c85ac188
Rosengren <kev.rosengren@gmail.com>
2015-03-20 01:46:13 -0400

    added tests for file reader

--- project/src/tests/FileInfoReaderTest.java
+++ project/src/tests/FileInfoReaderTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import districtServer.Candidate;
 import districtServer.FileInfoReader;
 import districtServer.Voter;
 import org.junit.Before;
@@ -7,9 +8,12 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.*;
+
 public class FileInfoReaderTest {
 
     private static final String DISTRICT_NAME = "Burlington";
+    private static final String INVALID_DISTRICT_NAME = "NotADistrict";
 
     private FileInfoReader reader;
 
@@ -21,8 +25,34 @@ public class FileInfoReaderTest {
     @Test
     public void testBuildVoterList() {
         ArrayList<Voter> voters = reader.buildVoterList(DISTRICT_NAME);
-        for (Voter v : voters) {
-            System.out.println(v.getLoginName());
-        }
+        assertFalse(voters.isEmpty());
+    }
+
+    @Test
+    public void testInvalidVoterList() {
+        ArrayList<Voter> voters = reader.buildVoterList(INVALID_DISTRICT_NAME);
+        assertTrue(voters.isEmpty());
+    }
+
+    @Test
+    public void testBuildCandidateList() {
+        ArrayList<Candidate> candidates = reader.buildCandidateList(DISTRICT_NAME);
+        assertFalse(candidates.isEmpty());
+    }
+
+    @Test
+    public void testInvalidCandidateList() {
+        ArrayList<Candidate> candidates = reader.buildCandidateList(INVALID_DISTRICT_NAME);
+        assertTrue(candidates.isEmpty());
+    }
+
+    @Test
+    public void testForValidDistrict() {
+        assertTrue(reader.isValidDistrictName(DISTRICT_NAME));
+    }
+
+    @Test
+    public void testForInvalidDistrict() {
+        assertFalse(reader.isValidDistrictName(INVALID_DISTRICT_NAME));
     }
 }
